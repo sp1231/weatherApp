9882ad0f513865e00aaea5536dfc96445c5d9655
Saswat Pattnayak <sp1231@pop-os.localdomain>
2022-09-25 01:23:43 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '309a519febmsh3085e91915f09ecp1766e2jsn3459e83dbbcd',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "91dcc57cd9c1c244710f86d37e4af69e";
+
